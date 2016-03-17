377a6dc0d5dfa3c47602c19f6d91f0f751ae0061
Andrey <karmatsky@yandex.ru>
2016-03-18 00:31:29 +0300

    available stations

--- js/citibike.js
+++ js/citibike.js
@@ -8,18 +8,14 @@ var start = { z: 12.5, center: [-73.991226,40.740523], bearing: -61, maxZoom: 17
     panel = d3.select("#panel"),
     panelHeader = d3.select("#panelHeader"),
     panelContent = d3.select("#panelContent"),
+    panelContentParams = d3.select("#panelContentParams"),
+    panelContentGraphs = d3.select("#panelContentGraphs"),
+    panelClose = d3.select("#panelClose"),
     interval, sliding,
     isCursor;
 
-var modes = {
-  total: { id: "total" },
-  hours: { id: "hours", slice: 'h0' },
-  station: { id: "station", citibike_id: 0, feature: {} },
-  direction: { id: "hours", startid: 0, endid: 0 }
-  };
 
-var currentMode = { id: "total" }, //init mode
-    prevMode = currentMode;
+var currentMode = { id: "total", slice: -1 }; //init mode
 
 
 function timeFormatter(t) {
@@ -49,11 +45,11 @@ master = new mapboxgl.Map({
       else l['interactive'] = false;
   });
 
-//  leftStyle = processStyle(masterStyle,'r_');
-//  rightStyle = processStyle(masterStyle,'l_');
-//  loadCompare();
+  panelClose.on('click', function() {
+    changeMode({id: 'total', slice: currentMode.slice});
+  });
 
-
+  //draw slider
   getSlider();
 
   //start application
@@ -72,7 +68,9 @@ master = new mapboxgl.Map({
              }
            });
            if(citibike_id) {
-             changeMode({id: 'station', citibike_id: citibike_id },feature);
+             changeMode({id: 'station', slice: currentMode.slice, feature: feature });
+           } else {
+             changeMode({id: 'total', slice: currentMode.slice});
            }
        });
 })
@@ -112,14 +110,7 @@ function getSlider() {
         if(!sliding) {
           sliding = true;
           interval = setInterval(function () {
-              if(value<0) {
-                changeMode({id: 'total'});
-              } else {
-                changeMode({id: 'hours', slice: 'h'+value});
-              }
-
-
-
+                changeMode({id: currentMode.id, slice: Math.round(value), feature: (currentMode.id == 'station') ? currentMode.feature : false });
               clearInterval(interval);
               sliding = false;
           }, 500);
@@ -131,80 +122,70 @@ function getSlider() {
       .on("slideend", function(evt, value) {
         sliding = false;
         clearInterval(interval);
-        if(value>0) changeMode({id: 'hours', slice: 'h'+Math.round(value)});
-          else changeMode({id: 'total'});
+          changeMode({id: currentMode.id, slice: Math.round(value), feature: (currentMode.id == 'station') ? currentMode.feature : false });
       })
   );
 
   d3.select("#handle-one").text('Total');
-
-
 }
 
-function processStyle(style, hide) {
-  //style;
-  var out = JSON.parse(JSON.stringify(style)); //bad bad hack;
-  out.name = hide + 'style';
-  out.layers.forEach(function(l,i) {
-    if (l.id.substr(0,2) === hide) {
-      //console.log(l);
-      l.layout.visibility = 'none';
-    }
-  });
-  return out;
-};
-
-  function changeMode(mode,feature) {
+  function changeMode(mode) {
 
     console.log(mode);
-    var line_filters = {}, slice, citibike_id;
+    var line_filters = {}, stations_filters = {}, slice, slice_st, sliceRate, citibike_id;
 
     //currentMode = mode;
 
-    console.log(slice);
+    if(mode.slice < 0) {
+      slice = 'total';
+      sliceRate = 1;
+      slice_st = 't_av';
 
-    if(mode.id == 'total') {
-      line_filters = {
-        l_line_1: ["all",["<","total",1000]],
-        l_line_2: ["all",[">=","total",1000],["<","total",1500]],
-        l_line_3: ["all",[">=","total",1500]],
-        r_line_1: ["all",["<","total",100]],
-        r_line_2: ["all",[">=","total",100],["<","total",150]],
-        r_line_3: ["all",[">=","total",150]]
+    } else {
+      slice = 'h' + mode.slice;
+      sliceRate = 0.15;
+      slice_st = 't' + mode.slice;
+    }
+
+
+    line_filters = {
+        l_line_1: ["all",[">",slice,0],["<",slice,700*sliceRate]],
+        l_line_2: ["all",[">=",slice,700*sliceRate],["<",slice,1200*sliceRate]],
+        l_line_3: ["all",[">=",slice,1500*sliceRate]],
+        r_line_1: ["all",[">",slice,0],["<",slice,70*sliceRate]],
+        r_line_2: ["all",[">=",slice,70*sliceRate],["<",slice,120*sliceRate]],
+        r_line_3: ["all",[">=",slice,120*sliceRate]]
       };
+
+    stations_filters = {
+      av_20: ["all",[">=",slice_st,0.1],["<",slice_st,0.35]],
+      av_50: ["all",[">=",slice_st,0.35],["<",slice_st,0.65]],
+      av_80: ["all",[">=",slice_st,0.65],["<",slice_st,0.9]],
+      av_100: ["all",[">=",slice_st,0.9]]
     }
 
-    if(mode.id == 'hours') {
-      slice = mode.slice;
+    console.log(stations_filters);
 
-      modes['hours'] = mode; //setting the current slice
 
-      line_filters = {
-        l_line_1: ["all",["<", slice,50]],
-        l_line_2: ["all",[">=",slice,50],["<",slice,100]],
-        l_line_3: ["all",[">=",slice,100]],
-        r_line_1: ["all",["<",slice,20]],
-        r_line_2: ["all",[">=",slice,20],["<",slice,50]],
-        r_line_3: ["all",[">=",slice,50]]
-        }
-    }
 
     if(mode.id == 'station') {
-      slice = "total";
-      citibike_id = mode.citibike_id;
-      console.log(mode);
+      citibike_id = mode.feature.properties.citibike_id;
+      var stationRate = 0.3;
       line_filters = {
-        l_line_1: ["all",["==", "startid",0]],
-        l_line_2: ["all",["<",slice,700],["==", "startid",citibike_id]],
-        l_line_3: ["all",[">=",slice,700],["==", "startid",citibike_id]],
-        r_line_1: ["all",["==", "startid",0]],
-        r_line_2: ["all",["<",slice,70],["==", "startid",citibike_id]],
-        r_line_3: ["all",[">=",slice,70],["==", "startid",citibike_id]]
-      }
+          l_line_1: ["all",["<",slice,500*sliceRate*stationRate], ["==", "startid",citibike_id]],
+          l_line_2: ["all",[">=",slice,500*sliceRate*stationRate],["<","total",1000*sliceRate*stationRate], ["==", "startid",citibike_id]],
+          l_line_3: ["all",[">=",slice,1000*sliceRate*stationRate], ["==", "startid",citibike_id]],
+          r_line_1: ["all",["<",slice,50*sliceRate*stationRate], ["==", "endid",citibike_id]],
+          r_line_2: ["all",[">=",slice,50*sliceRate*stationRate],["<","total",100*sliceRate*stationRate], ["==", "endid",citibike_id]],
+          r_line_3: ["all",[">=",slice,100*sliceRate*stationRate], ["==", "endid",citibike_id]]
+        };
+
+
 
       master.setLayoutProperty("s_stations_selection", "visibility", "visible");
       master.setFilter("s_stations_selection", ["all",["==", "citibike_id",citibike_id]]);
-
+    } else {
+      master.setLayoutProperty("s_stations_selection", "visibility", "none");
     }
 
     //apply
@@ -212,16 +193,19 @@ function processStyle(style, hide) {
       master.batch(function (batch) {
         for(lf in line_filters) {
             batch.setFilter(lf, line_filters[lf]);
-      }
+        }
+        for(st in stations_filters) {
+          batch.setFilter(st, stations_filters[st]);
+          console.log(st + ': ' + JSON.stringify(stations_filters[st]));
+        }
     });
     }
 
     //setting the currentMode
-    prevMode = currentMode;
     currentMode = mode;
 
-  if(feature) {
-    getPanel(feature);
+  if(mode.feature) {
+    getPanel(mode.feature);
     panel.style("display", "block");
   } else {
     panel.style("display", "none");
@@ -230,8 +214,9 @@ function processStyle(style, hide) {
 }
 
 function getPanel(feature) {
-  panelHeader.text(feature.properties.label)
-  panelContent.text('content');
+  panelHeader.text('#' + feature.properties.citibike_id + ': '+ feature.properties.label)
+  console.log(feature.properties);
+  panelContentParams.html('<span class="trips">Outgoing trips:&nbsp;'+ feature.properties.outgoing_trips + '</span>&nbsp;&nbsp;<span class="balanced">' + feature.properties.incoming_balancing + '</span>');
 
 
 }
